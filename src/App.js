5f178abe4f9ee7e345f35781464ca4149538c53a
LouisAvo <24avondetl@asij.ac.jp>
2022-06-14 14:57:43 +0900

    Added a greeting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Hellooooo
+           Hi I am Louis
         </p>
         <a
           className="App-link"
