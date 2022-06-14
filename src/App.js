0f2cd348fde67b46bb315b284b43e31489b4bc0f
LouisAvo <24avondetl@asij.ac.jp>
2022-06-14 15:20:25 +0900

    added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Hi I am Louis
+           Hi I am Louis I am copying
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This website
         </a>
       </header>
     </div>
